a600ce61ace0781ccb77ca801dd4f05e9601c84b
mcr229 <max.ren2234@gmail.com>
2019-11-03 16:44:29 -0500

    added the constant charging

--- acnportal/algorithms/__init__.py
+++ acnportal/algorithms/__init__.py
@@ -1,3 +1,3 @@
 from .base_algorithm import BaseAlgorithm
-from .uncontrolled_charging import UncontrolledCharging
+from .uncontrolled_charging import *
 from .sorted_algorithms import *
--- acnportal/algorithms/uncontrolled_charging.py
+++ acnportal/algorithms/uncontrolled_charging.py
@@ -34,3 +34,37 @@ class UncontrolledCharging(BaseAlgorithm):
         for ev in active_evs:
             schedule[ev.station_id] = [self.interface.max_pilot_signal(ev.station_id)]
         return schedule
+
+class ConstantCharging(BaseAlgorithm):
+    """ Simple algorithm for uncontrolled EV charging.
+
+    Implements abstract class BaseAlgorithm.
+
+    This algorithm ignores all infrastructure constraints.
+    All EVs will be charged as quickly as possible according to their maximum charging rate and their battery dynamics.
+
+    """
+    def __init__(self, const_value):
+        super().__init__()
+        self.const = const_value  # Call algorithm each period since it only returns a rate for the next period.
+
+    def schedule(self, active_evs):
+        """ Schedule each EV to charge as quickly as possible ignoring all infrastructure constraints.
+
+        Implements abstract method schedule from BaseAlgorithm.
+
+        See class documentation for description of the algorithm.
+
+        This algorithm SHOULD NOT be run on physical infrastructure which has safety constraints as it could
+        (and likely will) violate those constraints.
+
+        Args:
+            active_evs (List[EV]): see BaseAlgorithm
+
+        Returns:
+            Dict[str, List[float]]: see BaseAlgorithm
+        """
+        schedule = {}
+        for ev in active_evs:
+            schedule[ev.station_id] = [self.const_value]
+        return schedule
